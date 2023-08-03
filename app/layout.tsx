2830c58718edc2a9908acbb23b572e86e6161c8e
Adam Zhao <adamzhao@stanford.edu>
2023-08-02 20:16:48 -0400

    more unecessary imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Providers from './providers'
-import ThemeSwitcher from './ThemeSwitcher'
 
 
 const inter = Inter({ subsets: ['latin'] })
