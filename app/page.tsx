32f00fb58215571a2ed2044fc8c5820a491d8e90
Adam Zhao <adamzhao@stanford.edu>
2023-08-03 21:46:51 -0400

    hover colors

--- app/page.tsx
+++ app/page.tsx
@@ -25,34 +25,44 @@ export default function Home() {
                     <Image src="/moon.svg" alt="Moon in Dark Mode" width={75} height={75} />
                 </div>
             </div>
-
+            {/* <Link href="/about">
+  <div className="group rounded-lg border px-5 py-4 transition-colors hover:border-gray-300 hover:bg-green-100">
+    <h2 className="mb-3 text-xl font-semibold opacity-75">
+      About
+    </h2>
+    <p className="m-0 text-sm opacity-50">
+      An intro to me.
+    </p>
+  </div>
+</Link> */}
             <div className="gap-6 sm:gap-12 grid text-center lg:mb-0 grid-cols-3 lg:text-left">
-    {[
-        { title: "About", description: "An intro to me.", color: "green", link: "/about" },
-        { title: "Technical", description: "Technical background.", color: "pink", link: "/exp" },
-        { title: "More", description: "Non-technical tidbits.", color: "orange", link: "/other" }
-    ].map((item, index) => {
-        return (
-            <Link
-                key={index}
-                href={`${item.link}`}
-                className={`group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-${item.color}-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full lg:w-[220px] flex flex-col items-center`}
-            >
-                <div className="flex items-center justify-center w-full space-x-2">
-                    <h2 className={`font-semibold opacity-75 text-xs lg:text-2xl`}>
-                        {item.title}
-                    </h2>
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none whitespace-nowrap">
-                        -&gt;
-                    </span>
-                </div>
-                <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
-                    {item.description}
-                </p>
-            </Link>
-        );
-    })}
-</div>
+                {[
+                    { title: "About", description: "An intro to me.", color: "green", link: "/about" },
+                    { title: "Technical", description: "Technical background.", color: "pink", link: "/exp" },
+                    { title: "More", description: "Non-technical tidbits.", color: "orange", link: "/other" }
+                ].map((item, index) => {
+                    return (
+                        <Link
+                            key={index}
+                            href={`${item.link}`}
+                            className={`group rounded-lg border border-transparent px-5 py-4 transition-colors w-full lg:w-[220px] flex flex-col items-center hover:border-gray-300 hover:dark:border-neutral-700 ${item.color === 'green' ? 'hover:bg-green-100' : item.color === 'pink' ? 'hover:bg-pink-100' : 'hover:bg-orange-100'} hover:dark:bg-neutral-800/30`}
+                        >
+                            <div className="flex items-center justify-center w-full space-x-2">
+                                <h2 className={`font-semibold opacity-75 text-xs lg:text-2xl`}>
+                                    {item.title}
+                                </h2>
+                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none whitespace-nowrap">
+                                    -&gt;
+                                </span>
+                            </div>
+                            <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
+                                {item.description}
+                            </p>
+                        </Link>
+                    );
+                })}
+
+            </div>
         </main>
     )
 }
