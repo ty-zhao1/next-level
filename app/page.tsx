62a1f18da3b9602535305f622882206da41e7262
Adam Zhao <adamzhao@stanford.edu>
2023-08-03 21:33:20 -0400

    fixed hover issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24 bg-blue-100 dark:bg-olive">
             {/* <Nav/> */}
-            <SphereGrid/>
+            <SphereGrid />
             <div className="relative flex flex-col place-items-center mt-12 sm:mt-24">
                 <h1 className="text-4xl sm:text-7xl text-black dark:text-gray-300 font-bold">Adam Zhao</h1>
                 <p className="text-md sm:text-lg text-gray-700 dark:text-gray-400">Welcome to my website!</p>
             </div>
-            
+
             {/* Sun and Moon container */}
             <div className="flex justify-center mt-4 sm:mt-8 mb-8 sm:mb-16 scale-50 sm:scale-100">
                 {/* Sun Image for Light Mode */}
@@ -25,28 +25,29 @@ export default function Home() {
                     <Image src="/moon.svg" alt="Moon in Dark Mode" width={75} height={75} />
                 </div>
             </div>
-            
+
             <div className="gap-6 sm:gap-12 grid text-center lg:mb-0 grid-cols-3 lg:text-left">
-    {["About", "Technical", "More things"].map((title, index) => {
-        const descriptions = ["An intro to me.", "Technical background.", "Non-technical tidbits."];
-        const colors = ["green", "pink", "orange"];
-        const link = ["/about", "/exp", "/other"]
+    {[
+        { title: "About", description: "An intro to me.", color: "green", link: "/about" },
+        { title: "Technical", description: "Technical background.", color: "pink", link: "/exp" },
+        { title: "More", description: "Non-technical tidbits.", color: "orange", link: "/other" }
+    ].map((item, index) => {
         return (
             <Link
                 key={index}
-                href={link[index]}
-                className={`group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-${colors[index]}-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full lg:w-[220px] flex flex-col items-center`}
+                href={`${item.link}}`}
+                className={`group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-${item.color}-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full lg:w-[220px] flex flex-col items-center`}
             >
                 <div className="flex items-center justify-center w-full space-x-2">
                     <h2 className={`font-semibold opacity-75 text-xs lg:text-2xl`}>
-                        {title}
+                        {item.title}
                     </h2>
                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none whitespace-nowrap">
                         -&gt;
                     </span>
                 </div>
                 <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
-                    {descriptions[index]}
+                    {item.description}
                 </p>
             </Link>
         );
