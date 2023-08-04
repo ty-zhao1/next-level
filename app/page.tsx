6194a6b78658caa575ea493cfbb5beb3bd76ea7d
Adam Zhao <adamzhao@stanford.edu>
2023-08-03 20:32:32 -0400

    button help

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 >
                     <h2 className={`mb-3 text-2xl font-semibold opacity-75`}>
-                        Other things{' '}
+                        More{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
