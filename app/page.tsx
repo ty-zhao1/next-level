fe644b58e6feb5fa80bf61327fb8ed6d95f69315
Adam Zhao <adamzhao@stanford.edu>
2023-08-03 21:21:22 -0400

    even spaced

--- app/page.tsx
+++ app/page.tsx
@@ -26,52 +26,32 @@ export default function Home() {
                 </div>
             </div>
             
-            <div className="gap-x-6 sm:gap-x-12 grid text-center lg:mb-0 grid-cols-3 lg:text-left">
+            <div className="gap-6 sm:gap-12 grid text-center lg:mb-0 grid-cols-3 lg:text-left">
+    {["About", "Technical", "More things"].map((title, index) => {
+        const descriptions = ["An intro to me.", "Technical background.", "Non-technical tidbits."];
+        const colors = ["green", "pink", "orange"];
+        
+        return (
             <Link
-                    href="/about"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-green-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                >
-                    <h2 className={`mb-3 lg:text-2xl sm:text-xs font-semibold opacity-75`}>
-                        About{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
+                key={index}
+                href={`/${title.toLowerCase()}`}
+                className={`group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-${colors[index]}-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full lg:w-[220px] flex flex-col items-center`}
+            >
+                <div className="flex items-center justify-center w-full space-x-2">
+                    <h2 className={`font-semibold opacity-75 text-xs lg:text-2xl`}>
+                        {title}
                     </h2>
-                    <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
-                        An intro to me.
-                    </p>
-                </Link>
-
-                <Link
-                    href="/exp"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-pink-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                >
-                    <h2 className={`mb-3 lg:text-2xl  sm:text-xs  font-semibold opacity-75`}>
-                        Technical{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
-                        Technical background.
-                    </p>
-                </Link>
-
-                <Link
-                    href="/other"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                >
-                    <h2 className={`mb-3 lg:text-2xl sm:text-xs font-semibold opacity-75`}>
-                        More{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
-                        Non-technical tidbits.
-                    </p>
-                </Link>
-            </div>
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </div>
+                <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>
+                    {descriptions[index]}
+                </p>
+            </Link>
+        );
+    })}
+</div>
         </main>
     )
 }
