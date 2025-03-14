8b01b461f94f1a100f5ec57a8133ae43b6cec44c
Adam Zhao <adamzhao@stanford.edu>
2025-03-13 19:43:31 -0700

    some color/format changes to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,9 +4,13 @@ import Nav from '../../components/Nav';
 export default function Projects() {
     return (
         // say hello for now
-        <div className="min-h-screen bg-gray-700">
+        <div className="min-h-screen bg-stone-500">
             <Nav />
-            <p>Hello</p>
+            <div className="px-4 py-4 max-w-md mx-auto sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+                <h2 className="text-4xl font-extrabold text-gray-100">
+                    Projects
+                </h2>
+            </div>
         </div>
     )
 }
